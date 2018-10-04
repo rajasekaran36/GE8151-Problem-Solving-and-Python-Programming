0d918e2f70460951f8d9dc44fc19f010e87d54a1
Rajasekaran S <proffraja@gmail.com>
2018-10-04 14:23:23 +0530

    Last Update 04-10-2018 14:23:23.24

---
+++ Slides/Unit 2/operators.py
@@ -0,0 +1,87 @@
+a = 5
+b = 4
+print("Arithmatic")
+print("a:",a,"b:",b)
+print("a+b :",a+b)
+print("a-b :",a-b)
+print("a*b :",a*b)
+print("a/b :",a/b)
+print("a%b :",a%b)
+print("a**b :",a**b)
+print("a//b :",a//b)
+
+print("\n\n")
+print("Relational")
+print("a:",a,"b:",b)
+print("a<b :",a<b)
+print("a>b :",a>b)
+print("a<=b :",a<=b)
+print("a>=b :",a>=b)
+print("a==b :",a==b)
+print("a!=b :",a!=b)
+print("\n\n")
+print("Logical")
+a = True
+b = False
+print("a:",a,"b:",b)
+print ("a and b", a and b)
+print ("a or b", a or b)
+print ("not a", not a)
+print ("not b", not b)
+
+print("\n\n")
+print("Bitwise")
+a = 2
+b = 4
+print("a:",a,"b:",b)
+print("a&b", a&b)
+print("a|b",a|b)
+print("~a",~a)
+print("~b",~b)
+print("a^b",a^b)
+print("a>>b",a>>b)
+print("a<<b",a<<b)
+print("\n")
+a = 5
+b = 4
+print("Assigment Arithmatic")
+print("a:",a,"b:",b)
+a+=2
+print("a+=2 :",a)
+a-=2
+print("a-=2 :",a)
+a*=2
+print("a*=2 :",a)
+a/=2
+print("a/=2 :",a)
+a%=2
+print("a%=2 :",a)
+a**=2
+print("a**=2 :",a)
+a//=2
+print("a//=2 :",a)
+
+print("\n")
+a = 5
+b = 4
+print("Assigment Bitwise")
+print("a:",a,"b:",b)
+a&=2
+print("a&=2 :",a)
+a|=2
+print("a|=2 :",a)
+a!=2
+print("a!=2 :",a)
+a^=2
+print("a^=2 :",a)
+a>>=2
+print("a>>=2 :",a)
+a<<=2
+print("a<<=2 :",a)
+print("\n")
+print("Membership")
+a = [1,3,4,2,10]
+print("2 in a",2 in a)
+print("2 not in a",2 not in a)
+print("11 in a",11 in a)
+print("11 not in a",11 not in a)
